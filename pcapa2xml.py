bd8c71712772a2f8823b3158b5877240cbf46991
Anooyman <875734078@qq.com>
2016-04-06 11:59:51 +0800

    4月6日 TCP/IP实验课

--- pcapa2xml.py
+++ pcapa2xml.py
@@ -1,9 +1,26 @@
 #coding:utf-8
+
+import xml.dom.minidom
 import sqlite3
 
-def creatdb():
-    cx = sqlite3.connect("/home/anooy/python/test.db")
-    cx.execute('''CREATE TABLE PACKETS
-            ();''')
 
 if __name__ == '__main__':
+#    DOMTree = xml.dom.minidom.parse("123.xml") #    collection = DOMTree.documentElement
+#    proto = collection.getElementsByTagName("proto")
+#    for item in proto:
+#        if item.getAttribute("name") == "tcp":
+#            print item.getAttribute("name")
+#            print item.getAttribute("showname")
+#            print item.getAttribute("size")
+
+    connect = sqlite3.connect("tcp.db")
+    connect.execute('''CREATE TABLE TCP
+            SEQ INT PRIMARY KEY
+            SIP INT
+            DIP INT
+            SSORT INT
+            DSORT INT
+            PROTO STRING
+            SIZE INT 
+            TIME INT ''')
+
